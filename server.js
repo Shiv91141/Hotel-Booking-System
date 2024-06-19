4e5da63bbebdb73eec349ab9b0c451157e89e02d
Prasanna <me210003054@iiti.ac.in>
2024-06-19 17:44:40 +0530

    Connection to mongoDB Atlas and compass

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 
+const dbconfig = require("./db.js");
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running on port ${port}`));
