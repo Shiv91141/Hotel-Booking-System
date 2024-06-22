b70491e8e2b6aba86e4571d301e686894a30ae24
Shivam <me210003070@iiti.ac.in>
2024-06-22 12:48:55 +0530

    corrected useEffect hook code to fetch data properly

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,23 +1,26 @@
-import React ,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
 function Homescreen() {
-    const [rooms, setrooms]=useState([]);
-    useEffect(async()=>{
-        try {
-            const data=await axios.get('/api/rooms/getallrooms');
-            // console.log(data);
-            setrooms(data);
-        } catch (error) {
-            console.log(error);
-        }
-    },[])
-  return (
+    const [rooms, setrooms] = useState([]);
+    useEffect(() => {
+        const fetchRooms = async () => {
+            try {
+                const { data } = await axios.get('/api/rooms/getallrooms');
+                setrooms(data);
+            } catch (error) {
+                console.error('Error fetching rooms:', error);
+            }
+        };
+
+        fetchRooms();
+    }, []);
+return (
     <div>
         <h1>Home Screen</h1>
         <h1>there are {rooms.length} rooms</h1>
     </div>
-  )
+)
 }
 
 export default Homescreen
