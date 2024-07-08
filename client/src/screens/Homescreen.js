ca12d813eafa18ececd3b7430fa10338a8f568de
Prasanna <me210003054@iiti.ac.in>
2024-07-08 16:49:45 +0530

    added loading, error, success components

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,39 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import Room from '../components/Room';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Room from "../components/Room";
+import Loader from "../components/Loader";
+import Error from "../components/Error.js";
+import Success from "../components/Success";
 function Homescreen() {
-    const [rooms, setrooms] = useState([]);
-    const [loading, setloading] = useState();
-    const [error, seterror] = useState();
-    useEffect(() => {
-        const fetchRooms = async () => {
-            try {
-                setloading(true);
-                const { data } = await axios.get('/api/rooms/getallrooms');
-                setrooms(data);
-                setloading(false);
-            } catch (error) {
-                seterror(true);
-                console.error('Error fetching rooms:', error);
-                setloading(false);
-            }
-        };
+  const [rooms, setrooms] = useState([]);
+  const [loading, setloading] = useState();
+  const [error, seterror] = useState();
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        setloading(true);
+        const { data } = await axios.get("/api/rooms/getallrooms");
+        setrooms(data);
+        setloading(false);
+      } catch (error) {
+        seterror(true);
+        console.error("Error fetching rooms:", error);
+        setloading(false);
+      }
+    };
 
-        fetchRooms();
-    }, []);
-    return (
-        <div className='container'>
-            <div className="row justify-content-center mt-5">
-                {loading ? (<h1>Loading...</h1>) : error ? (<h1>Error</h1>) : (rooms.map((room) => {
-                    return (<div className="col-md-9 mt-2" key={room.id}>
-                        <Room room={room} />
-                    </div>)
-                }))}
-            </div>
-
-        </div>
-    )
+    fetchRooms();
+  }, []);
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        {loading ? (
+          <Loader />
+        ) : rooms.length >= 1 ? (
+          rooms.map((room) => {
+            return (
+              <div className="col-md-9 mt-3" key={room.id}>
+                <Room room={room} />
+              </div>
+            );
+          })
+        ) : (
+          <Error />
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Homescreen
+export default Homescreen;
--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoURL = process.env.MONGO_URL;
+const mongoURL =
+  "mongodb+srv://PrasannaKante:PrasannaKante@roomsserver.klsw6az.mongodb.net/?retryWrites=true&w=majority&appName=RoomsServer";
 
-mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose
+  .connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("MongoDB connection successful"))
   .catch((err) => console.log("MongoDB connection failed: ", err));
 
