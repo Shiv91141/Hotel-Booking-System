18495bfce71013ba2565d9984fd2740eae8c635a
Prasanna <me210003054@iiti.ac.in>
2024-08-22 15:45:03 +0530

    added frontend for login register

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar.js";
-import {BrowserRouter,Route,Routes,Link} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Homescreen from "./screens/Homescreen.js";
 import Bookingscreen from "./screens/Bookingscreen.js";
+import Registerscreen from "./screens/Registerscreen.js";
+import Loginscreen from "./screens/Loginscreen.js";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path='/home' exact Component={Homescreen}/>
-          <Route path='/book/:roomid' exact Component={Bookingscreen}/>
+          <Route path="/home" exact Component={Homescreen} />
+          <Route path="/book/:roomid" exact Component={Bookingscreen} />
+          <Route path="/register" exact Component={Registerscreen} />
+          <Route path="/login" exact Component={Loginscreen} />
         </Routes>
       </BrowserRouter>
     </div>
