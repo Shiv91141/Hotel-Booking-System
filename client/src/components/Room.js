904c5b8d7388be4da42b85747b40d22a9e3d865e
Prasanna <me210003054@iiti.ac.in>
2024-08-25 17:27:47 +0530

    added date feature

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Carousel from 'react-bootstrap/Carousel';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
 
-function Room({ room }) {
+function Room({ room, fromdate, todate }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
-    <div className='row bs'>
+    <div className="row bs">
       <div className="col-md-4">
-        <img src={room.imageurls[0]} className='smallimg' />
+        <img src={room.imageurls[0]} className="smallimg" />
       </div>
       <div className="col-md-7">
         <h1>{room.name}</h1>
@@ -21,11 +21,9 @@ function Room({ room }) {
           <p>Phone Number: {room.phonenumber}</p>
           <p>Type :{room.type}</p>
         </b>
-        <div style={{ float: 'right' }}>
-          <Link to={`/book/${room._id}`}>
-            <button className="btn btn-primary m-2">
-              Book Now
-            </button>
+        <div style={{ float: "right" }}>
+          <Link to={`/book/${room._id}/${fromdate}/${todate}`}>
+            <button className="btn btn-primary m-2">Book Now</button>
           </Link>
           <button className="btn btn-primary" onClick={handleShow}>
             View Details
@@ -33,33 +31,30 @@ function Room({ room }) {
         </div>
       </div>
 
-        <Modal show={show} onHide={handleClose} size='lg'>
-          <Modal.Header closeButton>
-            <Modal.Title>{room.name}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Carousel>
-
-              {room.imageurls.map(url => {
-                return <Carousel.Item>
-                  <img className='d-block bigimg'
-                    src={url}
-                    alt='First image' />
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{room.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel>
+            {room.imageurls.map((url) => {
+              return (
+                <Carousel.Item>
+                  <img className="d-block bigimg" src={url} alt="First image" />
                 </Carousel.Item>
-              })}
-            </Carousel>
-            <p>{room.description}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+              );
+            })}
+          </Carousel>
+          <p>{room.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
-
-      )
-
+  );
 }
 
-      export default Room
+export default Room;
